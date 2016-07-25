015ff6c554a46112e97611b3d34e9ccbf180a5d7
LibbyChapman <libbyc@incuna.com>
2016-07-25 11:23:13 +0100

    proper incuna style class name

--- scripts/style-guide.js
+++ scripts/style-guide.js
@@ -2,8 +2,8 @@
     'use strict';
 
     $(document).ready(function () {
-        // Add a class 'toggleItem' to elements to have them toggle a class of 'open' when clicked (to demonstrate show/hide effects etc)
-        $(document).on('click', '.toggleItem', function (e) {
+        // Add a class 'toggle-item' to elements to have them toggle a class of 'open' when clicked (to demonstrate show/hide effects etc)
+        $(document).on('click', '.toggle-item', function (e) {
             e.preventDefault();
             $(this).toggleClass('open');
         });
