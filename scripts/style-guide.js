b2e0217cc3c4c1d9d5ca06fd544300896941c304
LibbyChapman <libbyc@incuna.com>
2016-07-25 11:15:15 +0100

    added comment

--- scripts/style-guide.js
+++ scripts/style-guide.js
@@ -2,6 +2,7 @@
     'use strict';
 
     $(document).ready(function () {
+        // Add a class 'toggleItem' to elements to have them toggle a class of 'open' when clicked (to demonstrate show/hide effects etc)
         $(document).on('click', '.toggleItem', function (e) {
             e.preventDefault();
             $(this).toggleClass('open');
