c77cd3fdb19d0dece12a02a94a4f5d7ab8b595fc
Paula Norris <thepandalion@gmail.com>
2017-05-18 17:09:52 +0100

    Make sidebar nav sticky

--- scripts/style-guide.js
+++ scripts/style-guide.js
@@ -2,6 +2,21 @@
     'use strict';
 
     $(document).ready(function () {
+
+        // Sticky navigation
+        var distanceY;
+        var stickOn = 431;
+        var stickyNav = $('.sticky-nav');
+
+        $(window).on('scroll', function () {
+            distanceY = window.pageYOffset || $(window).scrollTop();
+            if (distanceY > stickOn) {
+                stickyNav.addClass('stick-please');
+            } else {
+                stickyNav.removeClass('stick-please');
+            }
+        });
+
         // Add a class 'toggle-item' to elements to have them toggle a class of 'open' when clicked (to demonstrate show/hide effects etc)
         $(document).on('click', '.js-toggle-item', function (e) {
             e.preventDefault();
